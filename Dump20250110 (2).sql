ca44ff799220bb1794c094adba52f477ab15871e
Samadhi22 <nethusam2004@gmail.com>
2025-01-10 23:15:38 +0530

    Add files via upload

---
+++ Dump20250110 (2).sql
@@ -0,0 +1,270 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: universitymanagementsystem
+-- ------------------------------------------------------
+-- Server version	9.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `collegefee`
+--
+
+DROP TABLE IF EXISTS `collegefee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `collegefee` (
+  `rollno` varchar(20) DEFAULT NULL,
+  `course` varchar(20) DEFAULT NULL,
+  `branch` varchar(20) DEFAULT NULL,
+  `semester` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `collegefee`
+--
+
+LOCK TABLES `collegefee` WRITE;
+/*!40000 ALTER TABLE `collegefee` DISABLE KEYS */;
+/*!40000 ALTER TABLE `collegefee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fee`
+--
+
+DROP TABLE IF EXISTS `fee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fee` (
+  `course` varchar(20) DEFAULT NULL,
+  `semester1` varchar(20) DEFAULT NULL,
+  `semester2` varchar(20) DEFAULT NULL,
+  `semester3` varchar(20) DEFAULT NULL,
+  `semester4` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fee`
+--
+
+LOCK TABLES `fee` WRITE;
+/*!40000 ALTER TABLE `fee` DISABLE KEYS */;
+INSERT INTO `fee` VALUES ('DSE','300000','300000','300000','300000'),('DNE','300000','300000','300000','300000'),('DCSD','300000','300000','300000','300000');
+/*!40000 ALTER TABLE `fee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES ('admin','12345');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marks`
+--
+
+DROP TABLE IF EXISTS `marks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `marks` (
+  `rollno` varchar(20) DEFAULT NULL,
+  `semester` varchar(20) DEFAULT NULL,
+  `marks1` varchar(50) DEFAULT NULL,
+  `marks2` varchar(50) DEFAULT NULL,
+  `marks3` varchar(50) DEFAULT NULL,
+  `marks4` varchar(50) DEFAULT NULL,
+  `marks5` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marks`
+--
+
+LOCK TABLES `marks` WRITE;
+/*!40000 ALTER TABLE `marks` DISABLE KEYS */;
+INSERT INTO `marks` VALUES ('1533647','1 st Semester','67','90','78','89','80');
+/*!40000 ALTER TABLE `marks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `name` varchar(40) DEFAULT NULL,
+  `fname` varchar(40) DEFAULT NULL,
+  `rollno` varchar(20) DEFAULT NULL,
+  `dob` varchar(40) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `class_x` varchar(20) DEFAULT NULL,
+  `class_xii` varchar(20) DEFAULT NULL,
+  `course` varchar(20) DEFAULT NULL,
+  `branch` varchar(20) DEFAULT NULL,
+  `course_type` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES ('Nethmi Samadhi','Dhammika','1533647','Aug 22, 2004','23/90 maharagama','0778956345','SandaruYasas@gmail.com','86','95','DSE','Colombo','Full Time'),('Sadew Ranaraje','Sadew','15334340','Aug 22, 2004','23/90 maharagama','0778956345','SandaruYasas@gmail.com','98','78','DSE','Colombo','Full Time'),('Sandaru Yasas','Dhammika','15332355','Jan 15, 2002','23/90 maharagama','0778956345','SandaruYasas@gmail.com','90','78','DSE','Colombo','Full Time');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentleave`
+--
+
+DROP TABLE IF EXISTS `studentleave`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `studentleave` (
+  `rollno` varchar(20) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `duration` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentleave`
+--
+
+LOCK TABLES `studentleave` WRITE;
+/*!40000 ALTER TABLE `studentleave` DISABLE KEYS */;
+INSERT INTO `studentleave` VALUES ('1533647','Jan 7, 2025','Ful Day');
+/*!40000 ALTER TABLE `studentleave` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subject`
+--
+
+DROP TABLE IF EXISTS `subject`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subject` (
+  `rollno` varchar(20) DEFAULT NULL,
+  `semester` varchar(20) DEFAULT NULL,
+  `subject1` varchar(50) DEFAULT NULL,
+  `subject2` varchar(50) DEFAULT NULL,
+  `subject3` varchar(50) DEFAULT NULL,
+  `subject4` varchar(50) DEFAULT NULL,
+  `subject5` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES ('1533647','1 st Semester','ICS','MC','APF','DBMS','EAD');
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacher`
+--
+
+DROP TABLE IF EXISTS `teacher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacher` (
+  `name` varchar(40) DEFAULT NULL,
+  `fname` varchar(40) DEFAULT NULL,
+  `empId` varchar(20) DEFAULT NULL,
+  `dob` varchar(40) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `x` varchar(20) DEFAULT NULL,
+  `xii` varchar(20) DEFAULT NULL,
+  `qualification` varchar(20) DEFAULT NULL,
+  `branch` varchar(20) DEFAULT NULL,
+  `department` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` VALUES ('Sandaru Yasas','Dhammika','1014246','Jan 21, 2009','234/f maharagama','078563267','sanadaru 123@gmail.com','99','78','MBBS','Colombo','Network');
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teacherleave`
+--
+
+DROP TABLE IF EXISTS `teacherleave`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacherleave` (
+  `empId` varchar(20) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `duration` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacherleave`
+--
+
+LOCK TABLES `teacherleave` WRITE;
+/*!40000 ALTER TABLE `teacherleave` DISABLE KEYS */;
+INSERT INTO `teacherleave` VALUES ('1014246','Jan 15, 2025','Ful Day');
+/*!40000 ALTER TABLE `teacherleave` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-10 23:12:58
